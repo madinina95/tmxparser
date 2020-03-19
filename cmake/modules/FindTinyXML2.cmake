20212779cbcb42a50519759fd90c964b308e5848
Lionheart <madinina95@gmail.com>
2020-03-19 13:55:10 +0100

    [feat] clean the repo a little bit

---
+++ cmake/modules/FindTinyXML2.cmake
@@ -0,0 +1,20 @@
+# Copyright (c) 2014 Andrew Kelley
+# This file is MIT licensed.
+# See http://opensource.org/licenses/MIT
+
+# TINYXML2_FOUND
+# TINYXML2_INCLUDE_DIR
+# TINYXML2_LIBRARIES
+
+find_path(TINYXML2_INCLUDE_DIR NAMES tinyxml2.h)
+find_library(TINYXML2_LIBRARIES NAMES tinyxml2)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(TINYXML2 DEFAULT_MSG TINYXML2_LIBRARIES TINYXML2_INCLUDE_DIR)
+
+mark_as_advanced(TINYXML2_INCLUDE_DIR TINYXML2_LIBRARIES)
+
+# handle success
+if(TINYXML2_FOUND)
+    message(STATUS "Found TinyXml2 in ${TINYXML2_INCLUDE_DIR} and ${TINYXML2_LIBRARIES}")
+endif()
